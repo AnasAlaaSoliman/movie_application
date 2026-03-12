414eefc61ae5a361fbe0acad1830cf93656b0767
Anas_Lenovo <anasalaasoliman59@gmail.com>
2026-03-12 02:40:30 +0200

    phase 3: add (genres , suggested movie) in movie Details screen.

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      initialRoute: RouteName.onboarding_page,
+      initialRoute: RouteName.navigation_screen,
       onGenerateRoute: AppRouters.onGenerateRoute,
 
     );
